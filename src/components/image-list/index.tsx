a85b764309f5f0b374b4d68f7535297f67498266
Anton Chernous <czernous_a@hotmail.com>
2024-01-17 20:03:27 +0000

    fix: remove dummy images

--- src/components/image-list/index.tsx
+++ src/components/image-list/index.tsx
@@ -12,7 +12,7 @@ const ImageList: React.FC = () => {
       <Typography variant="h1" fontSize={48} fontWeight={600} sx={{ textTransform: 'capitalize' }} mb={4}>
         {ctx.isFetchingCars ? <Skeleton sx={{ maxWidth: 300, bgcolor: 'grey.700' }} /> : ctx?.selectedTag}
       </Typography>
-      <MUIIMageList cols={3} gap={8} rowHeight={410}>
+      <MUIIMageList cols={3} gap={8} rowHeight={ctx.isFetchingCars ? 410 : undefined}>
         {ctx.isFetchingCars
           ? new Array(9).fill(1).map((_, i) => (
               <ImageListItem key={i}>
@@ -22,7 +22,7 @@ const ImageList: React.FC = () => {
           : ctx.cars?.map((car) => (
               <ImageListItem key={car.url}>
                 <img
-                  src={`https://images.unsplash.com/photo-1489824904134-891ab64532f1?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`} // TODO: Replace with car.url when access issues are rectified
+                  src={`${car.url}.webp`} // TODO: Replace with car.url when access issues are rectified
                   alt={car.alt_description}
                   loading="lazy"
                 />
